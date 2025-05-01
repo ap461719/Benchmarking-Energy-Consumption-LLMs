2e76a5d14d6240797273fbd89fa032cfc42bb317
radhikap0107 <radhika.jaraxis@gmail.com>
2025-05-01 06:46:50 +0000

    wandb setup

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -3,6 +3,7 @@ import os
 import time
 import csv
 import torch
+import wandb
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from models.load_llama import load_llama
@@ -12,6 +13,7 @@ from zeus.monitor import ZeusMonitor
 def build_prompt(sample, dataset_name):
     if dataset_name == "alpaca":
         prompt = sample["instruction"]
+
         if sample.get("input"):
             prompt += " " + sample["input"]
     elif dataset_name == "gsm8k":
@@ -91,16 +93,17 @@ def define_test_suites():
 #     ]
 
 
-def run_experiment(model_name, context_len, tokenizer, model, dataset_name, data, length_label, prompt_len, gen_tokens, batch_size, writer):
+def run_experiment(model_name, context_len, tokenizer, model, dataset_name, data, length_label, prompt_len, gen_tokens, batch_size, writer, wandb_run):
     device = next(model.parameters()).device
     for i in range(0, len(data[:8]), batch_size):
         batch = data[i:i+batch_size]
+        batch_number = i//batch_size + 1
         prompts = [build_prompt(s, dataset_name) for s in batch]
 
         if not any(prompts):
             continue
 
-        print(f"\nRunning: {model_name} | {dataset_name} | {length_label} | Batch #{i//batch_size + 1}")
+        print(f"\nRunning: {model_name} | {dataset_name} | {length_label} | Batch #{batch_number}")
 
         torch.cuda.empty_cache()
         torch.cuda.reset_peak_memory_stats()
@@ -115,8 +118,9 @@ def run_experiment(model_name, context_len, tokenizer, model, dataset_name, data
             truncation=True,
             max_length=prompt_len
         ).to(device)
-        input_token_lens = [len(seq) for seq in inputs["input_ids"]]
-        print("input token lengths: ", input_token_lens)
+
+        input_token_lens = [len(seq) for seq in inputs["input_ids"]] # len(seq) == prompt_len for each seq
+        print("input token lengths: ", input_token_lens)  
 
         if any(in_len + gen_tokens > context_len for in_len in input_token_lens):
             print(f"Skipping batch {i//batch_size + 1} due to context length limit.")
@@ -129,6 +133,7 @@ def run_experiment(model_name, context_len, tokenizer, model, dataset_name, data
             torch.isinf(inputs["input_ids"]).any() or
             (inputs["input_ids"] < 0).any()):
             continue
+
         print("max new tokens argument: ", gen_tokens)
         start = time.time()
         with torch.no_grad():
@@ -142,10 +147,10 @@ def run_experiment(model_name, context_len, tokenizer, model, dataset_name, data
         energy = round(measurement.total_energy, 2)
 
         pad_token_id = tokenizer.pad_token_id or tokenizer.eos_token_id
-        num_input_tokens = (inputs["input_ids"] != pad_token_id).sum().item()
+        num_input_tokens = (inputs["input_ids"] != pad_token_id).sum().item()                               # total input tokens in a batch without padding
         input_lengths_batch = [len(x) for x in inputs["input_ids"]]
-        output_lengths_batch = [len(seq) - in_len for seq, in_len in zip(output, input_lengths_batch)]
-        num_output_tokens = sum(output_lengths_batch)
+        output_lengths_batch = [len(seq) - in_len for seq, in_len in zip(output, input_lengths_batch)]      # num new output tokens (excluding input tokens) generated
+        num_output_tokens = sum(output_lengths_batch)                                                       # total output tokens in a batch without padding 
 
         power_data = getattr(measurement, "power_data", [])
         power_values = [p["power"] for p in power_data if "power" in p]
@@ -158,11 +163,27 @@ def run_experiment(model_name, context_len, tokenizer, model, dataset_name, data
         print("Before Padding input token counts:", input_token_counts)
 
         writer.writerow([
-            model_name, context_len, dataset_name, prompt_len, gen_tokens,
+            model_name, context_len, dataset_name, batch_number, prompt_len, gen_tokens, 
             latency, memory, energy, avg_power,
             num_input_tokens, num_output_tokens,
             energy_per_input, energy_per_output
         ])
+
+        wandb_run.log({
+            "batch_size": batch_size,
+            "batch_number": batch_number,
+            "prompt_len": prompt_len,
+            "output_len": gen_tokens,
+            "latency_sec": latency,
+            "memory_mb": memory,
+            "energy_j": energy,
+            "avg_power_w": avg_power,
+            "input_tokens": num_input_tokens,
+            "output_tokens": num_output_tokens,
+            "energy_per_input_token": energy_per_input,
+            "energy_per_output_token": energy_per_output,
+        })
+
 
         print(f" Input tokens actually used: {num_input_tokens} | Output tokens generated: {num_output_tokens}")
         print(f"Done | Latency: {latency:.2f}s | Mem: {memory:.0f}MB | "
@@ -185,10 +206,14 @@ def main():
     with open("results/metrics_output.csv", "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerow([
-            "Model", "Context_Window", "Dataset", "Prompt_Length", "Output_Length",
+            "Model", "Context_Window", "Dataset", "Batch_Number", "Prompt_Length", "Output_Length", 
             "Latency_sec", "Memory_MB", "Energy_J", "Avg_Power_W",
             "Input_Tokens", "Output_Tokens", "Energy_per_InputToken", "Energy_per_OutputToken"
         ])
+
+        print("\n++++++++++++++++++++++++++++++++")
+        print("Starting experiments...")
+        print("++++++++++++++++++++++++++++++++\n")
 
         for suite in test_suites:
             model_name = suite["model"]
@@ -209,7 +234,9 @@ def main():
                     print(f"Failed to load {model_name}: {e}")
                     continue
 
-            print(f"\nRUNNING SUITE: {suite['suite_name']}")
+            print(f"\n=======================================")
+            print(f"RUNNING SUITE: {suite['suite_name']} ... ")
+            print(f"=======================================\n")
 
             length_mapping = {"short": 128, "medium": 512, "long": 1024}
             prompt_len = length_mapping[suite["input_length"]]
@@ -217,11 +244,44 @@ def main():
 
             try:
                 test_name = f"input{prompt_len}_output{gen_tokens}"
+
+                run = wandb.init(
+                project="llm-inference-energy-benchmarking",
+                name=f"{test_name}-{time.strftime('%Y%m%d-%H%M%S')}",
+                reinit=True,
+                config={
+                    "suite": suite["suite_name"],
+                    "test_name": test_name,
+                    "model": model_name,
+                    "dataset": dataset_name,
+                    "batch_size": batch_size,
+                    "context_window": context_len,
+                    "prompt_length": prompt_len,
+                    "output_length": gen_tokens,
+                    "device": torch.cuda.get_device_name(torch.cuda.current_device()),
+                    "gpu_memory_total_gb": torch.cuda.get_device_properties(0).total_memory / 1e9
+                    }
+                )
+
                 run_experiment(model_name, context_len, tokenizer, model, dataset_name,
-                               data, test_name, prompt_len, gen_tokens, batch_size, writer)
+                               data, test_name, prompt_len, gen_tokens, batch_size, writer, run)
+
+                wandb.finish()
+
+                print(f"\n=======================================")
+                print(f"Finished running {suite['suite_name']} ...")
+                print(f"=======================================\n")
+                
             except Exception as e:
                 print(f"Experiment error: {e}")
                 continue
+
+        f.flush()
+        os.fsync(f.fileno())
+
+    print("\n++++++++++++++++++++++++++++++++")
+    print("All experiments completed!")
+    print("++++++++++++++++++++++++++++++++\n")
 
 if __name__ == "__main__":
     main()
